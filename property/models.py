d64fe116c8349a74509faba1a9067c84fef1a033
Sergey Morozov <se.mrzv@gmail.com>
2022-04-05 17:59:56 +0300

    setup admin UI for likes

--- property/models.py
+++ property/models.py
@@ -46,6 +46,13 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
 
+    liked_by = models.ManyToManyField(
+        User,
+        related_name="liked_flats",
+        blank=True,
+        null=True,
+        verbose_name="Кто лайкнул")
+
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
