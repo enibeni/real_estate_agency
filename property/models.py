113bce201f6c6574b3edafe9a533941c8133e4d1
Sergey Morozov <se.mrzv@gmail.com>
2022-04-08 16:40:27 +0300

    add Owner model and migration for it

--- property/models.py
+++ property/models.py
@@ -71,8 +71,11 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    name = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField(blank=True, verbose_name="Нормализованный номер владельца")
-    flats = models.ManyToManyField(Flat, related_name="owners", verbose_name="Квартиры в собственности")
+    flats = models.ManyToManyField(
+        Flat,
+        related_name="flat_owners",
+        verbose_name="Квартиры в собственности")
 
