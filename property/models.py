9ed0b7c3ba7c33d6a876d745cb27efce02f29382
Sergey Morozov <se.mrzv@gmail.com>
2022-04-08 12:42:46 +0300

    Create model Owner

--- property/models.py
+++ property/models.py
@@ -69,3 +69,10 @@ class Complaint(models.Model):
     flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name="Кваритра, на которую пожаловолись")
     complaint = models.TextField('Текст жалобы')
 
+
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField(blank=True, verbose_name="Нормализованный номер владельца")
+    flats = models.ManyToManyField(Flat, related_name="owners", verbose_name="Квартиры в собственности")
+
